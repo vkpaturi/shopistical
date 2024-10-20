999060ab4a49f19c3bd01a93e9eda2c39c68550f
Vamsi Krishna <vamsipvk2203@gmail.com>
2024-10-20 17:00:30 +0530

    Initial Commit

---
+++ src/main/java/com/project/spring/shopistical/ShopisticalApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring.shopistical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopisticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopisticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/spring/shopistical/ShopisticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring.shopistical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopisticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
